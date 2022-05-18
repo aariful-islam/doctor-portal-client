6c92024616235f1c201e135a3b0368c80c14a083
arif99050 <arifulislam99050@gmail.com>
2022-05-18 10:07:43 +0600

    style on available service

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -3,15 +3,26 @@ import React from 'react';
 const Service = ({service}) => {
     const {name,slots}=service
     return (
-        <div>
+        <div className='mb-12'>
             <div class="card lg:max-w-lg bg-base-100 shadow-xl">
   <div class="card-body">
     <h2 class="card-title">{name}</h2>
     <p> {slots.length} Available slots</p>
     
-    <p>{slots}</p>
-    <div class="card-actions justify-center bottom-0">
-      <button class="btn btn-primary">Book Appointment</button>
+    <p>
+        {
+            slots.length
+            ?
+            <span>{slots[0]}</span>
+            :
+            <span>No slot avaible</span>
+
+        }
+        
+    </p>
+    
+    <div class="card-actions justify-center ">
+      <button disabled={slots.length===0}  class="btn btn-primary mt-4">Book Appointment</button>
     </div>
   </div>
 </div>
