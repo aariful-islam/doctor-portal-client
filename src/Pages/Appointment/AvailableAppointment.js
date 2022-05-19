3879c963bb61f66af3cf4e62df2dc65a2683c9ad
arif99050 <arifulislam99050@gmail.com>
2022-05-19 16:19:30 +0600

    set slots time and service name on modal dynamicaaly

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -26,7 +26,9 @@ const AvailableAppointment = ({ date }) => {
         }
 
       </div>
-      {treatment && <BookingModal treatment={treatment}></BookingModal>}
+      {treatment && <BookingModal
+      date={date}
+      treatment={treatment}></BookingModal>}
 
     </div>
   );
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,26 +1,36 @@
 import React from "react";
-
-const BookingModal = ({ treatment }) => {
-const {name,slots}=treatment
+import { format } from "date-fns";
+const BookingModal = ({ treatment, date }) => {
+  const { name, slots } = treatment
   return (
     <div>
       <input type="checkbox" id="booking-modal" class="modal-toggle" />
-      
+
       <div class="modal modal-bottom sm:modal-middle">
-      
+
         <div class="modal-box">
-        <label for="booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+          <label for="booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
           <h3 class="font-bold text-lg">Booking for {name}
 
           </h3>
-          <p class="py-4">
+          <form action="" className="grid grid-cols-1 gap-3 justify-items-center mt-4">
+
+            <input type="text" disabled value={format(date, "PP")} class="input input-bordered w-full max-w-xs" />
+            <select class="select select-bordered w-full max-w-xs">
+              {
+                slots.map(slot=><option value={slot}>{slot}</option>)
+              }
+              
+            </select>
+            <input type="email" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+            <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+            <input type="submit" placeholder="Type here" class="btn btn-primary" />
+          </form>
+
+          {/* <p class="py-4">
             {slots}
-          </p>
-          <div class="modal-action">
-            <label for="booking-modal" class="btn">
-              Yay!
-            </label>
-          </div>
+          </p> */}
+
         </div>
       </div>
     </div>
